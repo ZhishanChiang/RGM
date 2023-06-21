6db132771cc0000767110898b3c904f0d4a07199
ZhishanChiang <jzs20@mails.tsinghua.edu.cn>
2023-06-21 14:56:53 +0800

    test fork

--- 1_experiment_eval.sh
+++ 1_experiment_eval.sh
@@ -1,7 +1,7 @@
 pwd
 echo '----------start-----------'
 echo 'ModelNet40_eval:'
-python eval.py --cfg experiments/test_RGM_Seen_Clean_modelnet40_transformer.yaml
+#python eval.py --cfg experiments/test_RGM_Seen_Clean_modelnet40_transformer.yaml
 python eval.py --cfg experiments/test_RGM_Seen_Jitter_modelnet40_transformer.yaml
 python eval.py --cfg experiments/test_RGM_Seen_Crop_modelnet40_transformer.yaml
 python eval.py --cfg experiments/test_RGM_Unseen_Crop_modelnet40_transformer.yaml
